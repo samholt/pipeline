c4aae3d741d8666bd70ef1b8a4389cde175faddb
Shan Carter <shanjcarter@gmail.com>
2016-12-07 14:45:18 -0800

    merge back

--- process_example.js
+++ process_example.js
@@ -1,23 +1,5 @@
-"use strict";
-
-(function() {
-	if(typeof require !== "undefined") {
-		var fs = require("fs"),
-				jsdom = require("jsdom").jsdom;
-	}
-
-	var sample = function(doc, data) {
-
-	}
-
-	if(typeof module !== "undefined" && module.exports ) {
-    module.exports = sample;
-  }
-
-
-})();
-
-
+var fs = require("fs"),
+	jsdom = require("jsdom").jsdom;
 
 var markup = fs.readFileSync("data/index.html", "utf8");
 var doc = jsdom(markup);
