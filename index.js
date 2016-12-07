77387e9faea553a43f2f6e2135b8e29437d6f4c7
Shan Carter <shanjcarter@gmail.com>
2016-12-07 15:38:46 -0800

    Adding full pipeline

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 var fs = require("fs"),
 	jsdom = require("jsdom").jsdom;
 
-var markup = fs.readFileSync("index.html", "utf8");
-var doc = jsdom(markup);
+var metaTags = require("./processMetaTags.js");
+
+var transforms = [metaTags];
+
+var markup = fs.readFileSync("data/index.html", "utf8");
+var dom = jsdom(markup);
+var data = [];
+
+// Send the DOM through each transform
+transforms.forEach(function(fn) {
+	fn.call(this, dom, data);
+});
+
+// Log the transformed html
+console.log(dom.documentElement.innerHTML);
