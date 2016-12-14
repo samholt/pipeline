9449e979120dbcbd186244fc1b2ee41e58619fc5
Shan Carter <shanjcarter@gmail.com>
2016-12-14 12:59:52 -0800

    Cleaning up

---
+++ bin/modules/verify-post-data.js
@@ -0,0 +1,58 @@
+module.exports = (post) => {
+
+  var d = {};
+
+  // Descriptions
+  d.title = post.title;
+  d.description = post.description;
+
+  // paths
+  d.publishPath = post.publishPath;
+  d.url = "http://distill.pub/" + post.publishPath;
+  d.githubUrl = "https://github.com/" + post.githubRepo;
+
+  d.homepage = !post.noHomepage;
+
+  // Dates
+  let months = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
+  let zeroPad = (n) => { return n < 10 ? "0" + n : n; };
+
+  d.publishedDate = post.publishedDate;
+  d.publishedYear = d.publishedDate.getFullYear();
+  d.publishedMonth = months[d.publishedDate.getMonth()];
+  d.publishedDay = d.publishedDate.getDate();
+
+  d.publishedMonthPadded = zeroPad(d.publishedDate.getMonth() + 1);
+  d.publishedDayPadded = zeroPad(d.publishedDate.getDate());
+
+  d.volume = d.publishedDate.getFullYear() - 2015;
+  d.number = d.publishedDate.getMonth() + 1;
+
+  // Identifier
+  // d.slug = post.authors[0].lastName.toLowerCase() + post.firstPublishedYear + post.title.split(" ")[0].toLowerCase()
+
+  // List of authors
+  d.authors = JSON.parse(JSON.stringify(post.authors));
+
+  d.authors.forEach((a) => {
+    if (!a.firstName || !a.lastName || !a.personalURL || !a.affiliation || !a.affiliationURL) {
+      console.error("Author missing required field(s) in " + post.githubRepo);
+    }
+  })
+
+  // if (view.distill.authors.length  > 2) {
+  //   view.distill.concatenatedAuthors = view.distill.authors[0].lastName + ", et al.";
+  // } else if (view.distill.authors.length === 2) {
+  //   view.distill.concatenatedAuthors = view.distill.authors[0].lastName + " & " + view.distill.authors[1].lastName;
+  // } else if (view.distill.authors.length === 1) {
+  //   view.distill.concatenatedAuthors = view.distill.authors[0].lastName
+  // }
+
+  // view.distill.bibtexAuthors = view.distill.authors.map(function(author){
+  //   return author.lastName + ", " + author.firstName;
+  // }).join(" and ");
+  //
+
+
+  return d;
+}
