16ca690140da9ca83bfe77080500651f93018148
Shan Carter <shanjcarter@gmail.com>
2016-12-14 16:37:05 -0800

    Fixes

--- bin/modules/insert-meta-tags.js
+++ bin/modules/insert-meta-tags.js
@@ -1,88 +1,80 @@
 module.exports = (dom, data) => {
+  dom.querySelector("html").setAttribute("lang", "en")
   let head = dom.querySelector("head");
-  head.appendChild(el(dom, "meta", {"chartset": "utf-8"}));
 
-  if (!dom.querySelector("meta[name=viewport]")) {
-    head.appendChild(el(dom, "meta", {"name": "viewport", "content": "width=device-width, initial-scale=1"}));
+  if (!dom.querySelector("meta[charset]")) {
+    appendHtml(head, `<meta charset="utf-8">`);
   }
-  head.appendChild(el(dom, "meta", {"http-equiv": "X-UA-Compatible", "content": "IE=Edge,chrome=1"}));
-  head.appendChild(el(dom, "link", {"rel": "icon", "type": "image/png", "href": "/favicon.png"}));
-  head.appendChild(el(dom, "link", {"href": "/rss.xml", "rel": "alternate", "type": "application/rss+xml", "title": "Articles from Distill"}));
+  if (!dom.querySelector("meta[name=viewport]")) {
+    appendHtml(head, `<meta name="viewport" content="width=device-width, initial-scale=1">`);
+  }
 
-  head.appendChild(el(dom, "title", data.title));
-  head.appendChild(el(dom, "link", {"rel": "canonical", "href": data.url}));
+  appendHtml(head, `
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+    <link rel="icon" type="image/png" href="/favicon.png">
+    <link href="/rss.xml" rel="alternate" type="application/rss+xml" title="Articles from Distill">
+    <link rel="canonical" href="${data.url}">
+    <title>${data.title}</title>
+  `);
 
-  // <meta name="author" content="Carter, et al.">
+  appendHtml(head, `
+    <!--  https://schema.org/Article -->
+    <meta property="article:published" itemprop="datePublished" content="${data.publishedDate}" />
+    <meta property="article:modified" itemprop="dateModified" content="${data.updatedDate}" />
+  `);
+  data.authors.forEach((a) => {
+    appendHtml(head, `<meta property="article:author" content="${a.firstName} ${a.lastName}" />`)
+  });
 
-  // <!--  https://schema.org/Article -->
-  // <meta property="article:published" itemprop="datePublished" content="Tue Dec 06 2016 20:39:33 GMT-0800 (PST)" />
-  // <meta property="article:modified" itemprop="dateModified" content="Tue Dec 06 2016 20:39:33 GMT-0800 (PST)" />
-  // <meta property="article:author" content="Shan Carter" />
-  // <meta property="article:author" content="David Ha" />
-  // <meta property="article:author" content="Ian Johnson" />
-  // <meta property="article:author" content="Chris Olah" />
+  appendHtml(head, `
+    <!--  https://developers.facebook.com/docs/sharing/webmasters#markup -->
+    <meta property="og:type" content="article"/>
+    <meta property="og:title" content="${data.title}"/>
+    <meta property="og:description" content="${data.description}">
+    <meta property="og:url" content="${data.url}"/>
+    <meta property="og:image" content="${data.url}/thumbnail.png"/>
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:site_name" content="Distill" />
+  `);
 
-  // <!--  https://developers.facebook.com/docs/sharing/webmasters#markup -->
-  // <meta property="og:type" content="article"/>
-  // <meta property="og:title" content="Experiments in Handwriting with a Neural Network"/>
-  // <meta property="og:description" content="Several interactive visualizations of a generative model of handwriting. Some are fun, some are serious.">
-  // <meta property="og:url" content="http://distill.pub/2016/handwriting/"/>
-  // <meta property="og:image" content="http://distill.pub/2016/handwriting/thumbnail.png"/>
+  appendHtml(head, `
+    <!--  https://dev.twitter.com/cards/types/summary -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="${data.title}">
+    <meta name="twitter:description" content="${data.description}">
+    <meta name="twitter:url" content="${data.url}">
+    <meta name="twitter:image" content="${data.url}/thumbnail.png">
+    <meta name="twitter:image:width" content="560">
+    <meta name="twitter:image:height" content="295">
+  `);
 
-  // <!--  https://dev.twitter.com/cards/types/summary -->
-  // <meta name="twitter:card" value="summary_large_image">
-  // <meta name="twitter:title" content="Experiments in Handwriting with a Neural Network">
-  // <meta name="twitter:description" content="Several interactive visualizations of a generative model of handwriting. Some are fun, some are serious.">
-  // <meta name="twitter:url" content="http://distill.pub/2016/handwriting/">
-  // <meta name="twitter:image" content="http://distill.pub/2016/handwriting/thumbnail.png">
-  // <meta name="twitter:image:width" content="560">
-  // <meta name="twitter:image:height" content="295">
-
-  // <!--  https://scholar.google.com/intl/en/scholar/inclusion.html#indexing -->
-  // <meta name="citation_title" content="Experiments in Handwriting with a Neural Network">
-  // <meta name="citation_author" content="Carter, Shan">
-  // <meta name="citation_author" content="Ha, David">
-  // <meta name="citation_author" content="Johnson, Ian">
-  // <meta name="citation_author" content="Olah, Chris">
-  // <meta name="citation_publication_date" content="2016/12/06">
-  // <meta name="citation_journal_title" content="Distill">
-
-  //
-  //
-  // // <meta name="citation_title" content="Experiments in Handwriting with a Neural Network">
-  head.appendChild(el(dom, "meta", {name: "citation_title", content: data.title}));
-  // // <meta name="citation_author" content="Carter, Shan">
-  // // <meta name="citation_author" content="Ha, David">
-  // // ...
-  // data.authors.forEach(author => {
-  //   var author_name = author.lastName + ", " + author.firstName;
-  //   appendMeta({name: "citation_author", content: author_name});
-  // });
-  // // <meta name="citation_publication_date" content="2016/12/06">
-  // var date = new Date(data.firstPublished);
-  // var date_string = date.getFullYear() + "/" + date.getMonth() + "/" + date.getDate();
-  // appendMeta({name: "citation_publication_date", content: date_string});
-  // // <meta name="citation_journal_title" content="Distill">
-  // appendMeta({name: "citation_journal_title", content: data.journal.full_title});
-  // appendMeta({name: "citation_volume", content: data.volume});
-  // appendMeta({name: "citation_issue", content: data.issue});
-  head.appendChild(el(dom, "meta", {name: "citation_fulltext_html_url", content: data.url}));
-  // // TODO: Get DOI and ISSN
-  // appendMeta({name: "citation_doi", content: data.doi});
-  // appendMeta({name: "citation_issn", content: data.journal.issn});
-
+  appendHtml(head, `
+    <!--  https://scholar.google.com/intl/en/scholar/inclusion.html#indexing -->
+    <meta name="citation_title" content="${data.title}">
+    <meta name="citation_publication_date" content="${data.publishedYear}/${data.publishedMonthPadded}/${data.publishedDayPadded}">
+    <meta name="citation_journal_title" content="Distill">
+    <meta name="citation_fulltext_html_url" content="${data.url}">
+    <meta name="citation_volume" content="${data.volume}">
+    <meta name="citation_issue" content="${data.issue}">
+  `);
+  // <meta name="citation_doi" content="data.doi">
+  // <meta name="citation_issn" content="data.journal.issn">
+  data.authors.forEach((a) => {
+    appendHtml(head, `<meta name="citation_author" content="${a.lastName}, ${a.firstName}" />`)
+  });
 
   return dom;
 }
 
-function el(dom, tag, attributes) {
+function appendHtml(el, html) {
+  el.innerHTML += html;
+}
+
+function el(dom, tag, attributes, inner) {
   let element = dom.createElement(tag);
   Object.keys(attributes).forEach((key) => {
     element.setAttribute(key, attributes[key]);
   });
+  if (inner) element.textContent = inner;
   return element;
 }
-//
-// function appendMeta(params){
-//   head.appendChild(el(dom, "meta", params));
-// }
--- bin/modules/verify-post-data.js
+++ bin/modules/verify-post-data.js
@@ -1,4 +1,8 @@
-module.exports = (post) => {
+let d3 = require("d3-time-format");
+
+let RFC = d3.timeFormat("%a, %d %b %Y %H:%M:%S %Z");
+
+module.exports = (post, journal) => {
 
   var d = {};
 
@@ -8,79 +12,60 @@ module.exports = (post) => {
 
   // paths
   d.distillPath = post.distillPath;
+  d.githubPath = post.githubPath;
   d.url = "http://distill.pub/" + post.distillPath;
   d.githubUrl = "https://github.com/" + post.githubPath;
 
+  // Homepage
   d.homepage = !post.noHomepage;
 
   // Dates
+  // TODO: fix updated date
+  d.updatedDate = post.publishedDate;
   let months = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
   let zeroPad = (n) => { return n < 10 ? "0" + n : n; };
-
   d.publishedDate = post.publishedDate;
+  d.publishedDateRFC = RFC(d.publishedDate);
   d.publishedYear = d.publishedDate.getFullYear();
   d.publishedMonth = months[d.publishedDate.getMonth()];
   d.publishedDay = d.publishedDate.getDate();
-
   d.publishedMonthPadded = zeroPad(d.publishedDate.getMonth() + 1);
   d.publishedDayPadded = zeroPad(d.publishedDate.getDate());
-
   d.volume = d.publishedDate.getFullYear() - 2015;
-  d.number = d.publishedDate.getMonth() + 1;
+  d.issue = d.publishedDate.getMonth() + 1;
 
-  // Identifier
-  // d.slug = post.authors[0].lastName.toLowerCase() + post.firstPublishedYear + post.title.split(" ")[0].toLowerCase()
-
-  // List of authors
+  // Authors
   d.authors = JSON.parse(JSON.stringify(post.authors));
-
   d.authors.forEach((a) => {
     if (!a.firstName || !a.lastName || !a.personalURL || !a.affiliation || !a.affiliationURL) {
       console.error("Author missing required field(s) in " + post.githubPath);
     }
-  })
+  });
+  d.bibtexAuthors = d.authors.map(function(author){
+    return author.lastName + ", " + author.firstName;
+  }).join(" and ");
 
-  // if (view.distill.authors.length  > 2) {
-  //   view.distill.concatenatedAuthors = view.distill.authors[0].lastName + ", et al.";
-  // } else if (view.distill.authors.length === 2) {
-  //   view.distill.concatenatedAuthors = view.distill.authors[0].lastName + " & " + view.distill.authors[1].lastName;
-  // } else if (view.distill.authors.length === 1) {
-  //   view.distill.concatenatedAuthors = view.distill.authors[0].lastName
+  if (d.authors.length  > 2) {
+    d.concatenatedAuthors = d.authors[0].lastName + ", et al.";
+  } else if (d.authors.length === 2) {
+    d.concatenatedAuthors = d.authors[0].lastName + " & " + d.authors[1].lastName;
+  } else if (d.authors.length === 1) {
+    d.concatenatedAuthors = view.distill.authors[0].lastName;
+  }
+
+  // Identifier
+  // d.slug = post.authors[0].lastName.toLowerCase() + post.firstPublishedYear + post.title.split(" ")[0].toLowerCase()
+
+  // var name = post.distillPath.split("/")[1];
+  // if (post.doi_suffix != undefined){
+  //   // Is the DOI a duplicate?
+  //   if (dois.indexOf(post.doi_suffix) != -1)
+  //     throw Error("Duplicate DOI suffix: " + post.doi_suffix);
+  //   dois.push(post.doi_suffix);
+  //   // Save DOI if for this post
+  //   if (name == data.name)
+  //     data.doi = data.journal.doi + "." + ("0000" + post.doi_suffix).slice(-5);
   // }
-
-  // view.distill.bibtexAuthors = view.distill.authors.map(function(author){
-  //   return author.lastName + ", " + author.firstName;
-  // }).join(" and ");
-  //
-
-
-  // module.exports = function preprocess_data(data){
-  //   set_volume_issue(data);
-  //   handle_dois(data);
-  // }
-  //
-  // function set_volume_issue(data){
-  //   var date = new Date(data.firstPublished);
-  //   data.volume = date.getFullYear()-2015;
-  //   data.issue = date.getMonth();
-  // }
-  //
-  // function handle_dois(data) {
-  //   var dois = [];
-  //   data.journal.posts.forEach(post => {
-  //     var name = post.distillPath.split("/")[1];
-  //     if (post.doi_suffix != undefined){
-  //       // Is the DOI a duplicate?
-  //       if (dois.indexOf(post.doi_suffix) != -1)
-  //         throw Error("Duplicate DOI suffix: " + post.doi_suffix);
-  //       dois.push(post.doi_suffix);
-  //       // Save DOI if for this post
-  //       if (name == data.name)
-  //         data.doi = data.journal.doi + "." + ("0000" + post.doi_suffix).slice(-5);
-  //     }
-  //   });
-  // }
-
 
   return d;
 }
