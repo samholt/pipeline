45165b6cf6d083453744f2c81d99abb5cf29f6ea
Ludwig Schubert <ludwigschubert@google.com>
2018-01-10 14:14:15 +0100

    Try verifying whether webhooks come from github

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,10 @@
 const functions = require('firebase-functions');
 const request = require('request');
 
+const githubMiddleware = require('github-webhook-middleware')({
+  secret: functions.config().github.secret
+});
+
 function requestBuild(repo) {
   const repositoryToBuild = encodeURIComponent(repo);
   const url =  "https://api.travis-ci.org/repo/" + repositoryToBuild + "/requests";
@@ -35,50 +39,58 @@ function requestBuild(repo) {
 
 exports.githubWebhook = functions.https.onRequest((req, resp) => {
   console.log("Handling a webhook from github.");
-
-  const isPush = req.body.repository && req.body.commits;
-  const isRelease = req.body.action && req.body.action == "published";
   
-  if (isPush) {
-    const repo = req.body.repository;
-    console.log("We are seeing a push to '" + repo.name + "'.");
-
-    const isPost = repo.name.startsWith('post--');
-    const isPublic = !repo.private;
-    const isToMaster = req.body.ref.endsWith('master');
-    if ( isPost && isToMaster && isPublic ) {
-      console.log("Push is to master branch of a public post repo; triggering a build of /pipeline.");
-      // TODO: decide when to build staging instead
-      requestBuild("distillpub/pipeline");
-    } else {
-      console.log("Push was not to master branch of a public post repo; doing nothing.");
-    }
-  }
-  
-  if (isRelease) {
-    const repo = req.body.repository;
-    console.log("We are seeing a release on '" + repo.name + "'.");
+  githubMiddleware(req, resp, () => {
+    const isPush = req.body.repository && req.body.commits;
+    const isRelease = req.body.action && req.body.action == "published";
     
-    const isOnTemplate = repo.name = "template";
-    if (isOnTemplate) {
-      console.log("Release was on template; triggering a build of /pipeline to deploy that new version on the distill website.");
-      requestBuild("distillpub/pipeline");
-    } else {
-      console.log("Release was not on a repo we care about; doing nothing.");
-    }
-  }
+    if (isPush) {
+      const repo = req.body.repository;
+      console.log("We are seeing a push to '" + repo.name + "'.");
 
-  resp.send("OK");
+      const isPost = repo.name.startsWith('post--');
+      const isPublic = !repo.private;
+      const isToMaster = req.body.ref.endsWith('master');
+      if ( isPost && isToMaster && isPublic ) {
+        console.log("Push is to master branch of a public post repo; triggering a build of /pipeline.");
+        // TODO: decide when to build staging instead
+        requestBuild("distillpub/pipeline");
+      } else {
+        console.log("Push was not to master branch of a public post repo; doing nothing.");
+      }
+    }
+    
+    if (isRelease) {
+      const repo = req.body.repository;
+      console.log("We are seeing a release on '" + repo.name + "'.");
+      
+      const isOnTemplate = repo.name = "template";
+      if (isOnTemplate) {
+        console.log("Release was on template; triggering a build of /pipeline to deploy that new version on the distill website.");
+        requestBuild("distillpub/pipeline");
+      } else {
+        console.log("Release was not on a repo we care about; doing nothing.");
+      }
+    }
+
+    resp.send("OK");
+  });
+
 });
 
 
 exports.githubDraftsWebhook = functions.https.onRequest((req, resp) => {
   console.log("Handling a drafts webhook from github.");
-  const isPush = req.body.repository && req.body.commits;
-  if (isPush) {
-    const repo = req.body.repository;
-    console.log("We are seeing a push to '" + repo.name + "'. Requesting drafts rebuild.");
-    requestBuild("distillpub/drafts");
-  }
-  resp.send("OK");
+  
+  githubMiddleware(req, resp, () => {
+    const isPush = req.body.repository && req.body.commits;
+    if (isPush) {
+      const repo = req.body.repository;
+      console.log("We are seeing a push to '" + repo.name + "'. Requesting drafts rebuild.");
+      requestBuild("distillpub/drafts");
+    }
+    console.log("Not a push event, ignoring.");
+    resp.send("OK");
+  });
+  
 });
